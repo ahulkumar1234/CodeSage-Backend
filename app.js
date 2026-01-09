ca80affc24375089b52ca6ee445a77b9470bab52
Rahul Kumar <rahulkumar8340527941@gmail.com>
2026-01-09 09:54:00 +0530

    cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const generateRouter = require('./route/generate.route')
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://code-sage-frontend-two.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://code-sage-frontend-two.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
